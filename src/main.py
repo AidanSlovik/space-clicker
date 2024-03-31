70a6f95be7f9718b27ae9833c5871c920da05c9a
AidanSlovik <158332992+AidanSlovik@users.noreply.github.com>
2024-03-31 17:53:32 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 purple = (255,0, 255)
 
-while start == false
+while start == false:
    canvas.fill(blue)
    font = pygame.font.SysFont("Arial", 20)
    txtsurf = font.render(str(points), True, orange)
