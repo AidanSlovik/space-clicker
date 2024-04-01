370213881d24935c0ff533f06380194b8ae73369
AidanSlovik <158332992+AidanSlovik@users.noreply.github.com>
2024-03-31 18:10:09 -0600

    Update and rename SpaceClickererer.py to SpaceClickerererer.py

--- src/SpaceClickerererer.py
+++ src/SpaceClickerererer.py
@@ -26,7 +26,8 @@ while start == False:
       canvas.blit(txtsurf, (100, 100))
       if event.type == pygame.MOUSEBUTTONDOWN:
          start = True
-      
+   pygame.display.update()
+   
 while done == False:
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
